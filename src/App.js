627eb2d7c2a6c765310b11fecf22e85e0a29c00a
Maharufh Ali Mir <85804873+maharufh@users.noreply.github.com>
2022-07-26 16:01:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+
+function App() {
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get the job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get the job done2"
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "You need to go to the market to get the job done3"
+    },
+  ]
+  return (
+    <>  
+      <Header title = "My Todos List" searchBar = {false}/>
+      <Todos todos={todos}/>
+      <Footer/>
+   </>
+  );
+}
+
+export default App;
